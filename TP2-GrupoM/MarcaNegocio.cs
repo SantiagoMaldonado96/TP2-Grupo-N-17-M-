704c4ab93fbe42837ef3ef7c60fe10dbd88943e6
Santiago Maldonado <santy_gm96@hotmail.com>
2024-04-21 22:32:52 -0300

    Conexion a la base de datos de Marcas

---
+++ TP2-GrupoM/MarcaNegocio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TP2_GrupoM
+{
+    internal class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Id, Descripcion From MARCAS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.IdMarca = (int)lector["Id"];
+                    aux.NombreMarca = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
