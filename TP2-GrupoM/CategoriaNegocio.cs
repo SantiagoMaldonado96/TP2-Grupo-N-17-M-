28866408525ea943ca46219f65a0538c117aa1d1
Santiago Maldonado <santy_gm96@hotmail.com>
2024-04-21 22:21:01 -0300

    Conexion a la base de datos de Categorias

---
+++ TP2-GrupoM/CategoriaNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TP2_GrupoM
+{
+    internal class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Id, Descripcion From CATEGORIAS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.IdCategoria = (int)lector["Id"];
+                    aux.NombreCategoria = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
