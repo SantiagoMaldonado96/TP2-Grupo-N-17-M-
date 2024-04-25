ac219aca0a52fc598404eeb81cf37d5d882c9b14
Santiago Maldonado <santy_gm96@hotmail.com>
2024-04-24 21:22:22 -0300

    Clase ImagenNegocio

--- Dominio/Imagen.cs
+++ Dominio/Imagen.cs
@@ -9,6 +9,8 @@ namespace Dominio
     public class Imagen
     {
         public int IdImagen { get; set; }
+
+        public int IdArtciulo { get; set; }
         public string UrlImagen { get; set; }
     }
 }
---
+++ Negocio/ImagenNegocio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dominio;
+using System.Net;
+
+namespace Negocio
+{
+    internal class ImagenNegocio
+    {
+        public List<Imagen> listar()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Id, IdArticulo, ImagenUrl From Imagenes";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.IdImagen = (int)lector["Id"];
+                    aux.IdArtciulo = (int)lector["IdArticulo"];
+                    aux.UrlImagen = (string)lector["ImagenUrl"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
